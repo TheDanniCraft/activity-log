b9452cfcfdf568b5d57d3bf602c435cdce5576ae
8minus1Trout <151639906+8minus1Trout@users.noreply.github.com>
2024-10-11 04:09:12 +0000

    undo

--- src/utils/github.js
+++ src/utils/github.js
@@ -113,13 +113,13 @@ async function fetchAndFilterEvents() {
                 return event;
             })
             .slice(0, eventLimit);
-        break;
-        // if (filteredEvents.length < eventLimit) {
-        //     const additionalEvents = await fetchAllEvents();
-        //     allEvents = additionalEvents.concat(allEvents);
-        // } else {
-        //     break;
-        // }
+            
+        if (filteredEvents.length < eventLimit) {
+            const additionalEvents = await fetchAllEvents();
+            allEvents = additionalEvents.concat(allEvents);
+        } else {
+            break;
+        }
     }
 
     filteredEvents = filteredEvents.slice(0, eventLimit);
