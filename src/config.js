180725baf1c019cb81748c6e342878c833e6fbb4
qwillie64 <131954766+qwillie64@users.noreply.github.com>
2024-10-11 19:51:31 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -42,6 +42,7 @@ module.exports = {
     token: core.getInput('GITHUB_TOKEN', { required: true }),
     eventLimit: processEventLimit(core.getInput('EVENT_LIMIT')),
     style: processStyle(core.getInput('OUTPUT_STYLE')),
+    targetRepos: processIgnoreEvents(core.getInput('TARGET_REPOS')),
     ignoreEvents: processIgnoreEvents(core.getInput('IGNORE_EVENTS')),
     readmePath: core.getInput('README_PATH'),
     commitMessage: core.getInput('COMMIT_MESSAGE')
