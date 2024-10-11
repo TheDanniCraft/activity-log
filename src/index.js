e7d30a6d8aaf05678f4a25becc9e82c223f0c96f
qwillie64 <131954766+qwillie64@users.noreply.github.com>
2024-10-11 17:33:58 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 const { fetchAndFilterEvents } = require('./utils/github');
 const { updateReadme } = require('./utils/file');
-const { username, token, eventLimit, ignoreEvents, readmePath, commitMessage } = require('./config');
+const { username, token, eventLimit, targetRepos, ignoreEvents, readmePath, commitMessage } = require('./config');
 const core = require('@actions/core')
 
 // Main function to execute the update process
 async function main() {
     try {
-        const activity = await fetchAndFilterEvents({ username, token, eventLimit, ignoreEvents });
+        const activity = await fetchAndFilterEvents({ username, token, eventLimit, targetRepos, ignoreEvents });
         await updateReadme(activity, readmePath);
     } catch (error) {
         core.setFailed(`❌ Error in the update process: ${error.message}`);
