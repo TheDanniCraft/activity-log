cabcba5210dc818d70b6748e485b9adabd5e5726
yulin17-chang <yzhang242@gmail.com>
2024-10-18 14:28:38 +0800

    Update github.js

--- src/utils/github.js
+++ src/utils/github.js
@@ -73,7 +73,7 @@ async function fetchAllEvents() {
 
             // Check for API rate limit or pagination issues
             if (events.length === 0) {
-                core.warning('⚠️ No more events available.');
+                core.warning('⚠️ B_and_R-2: No more events available.');
                 break; // No more events to fetch
             }
 
