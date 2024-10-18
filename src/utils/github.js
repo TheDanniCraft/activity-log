e3fa2f645297d6d3c0d7856bf842d77061d461be
8minus1Trout <151639906+8minus1Trout@users.noreply.github.com>
2024-10-18 11:45:12 +0800

    Update github.js

--- src/utils/github.js
+++ src/utils/github.js
@@ -73,7 +73,7 @@ async function fetchAllEvents() {
 
             // Check for API rate limit or pagination issues
             if (events.length === 0) {
-                core.warning('⚠️ No more events available.');
+                core.warning('⚠️ Update2: No more events available.');
                 break; // No more events to fetch
             }
 
