19013c4b69d761784d83e8b5fe25935f8a96194c
HansLu2025 <B11209013@gapps.ntust.edu.tw>
2025-10-10 21:46:49 +0800

    Add python test file

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -5,7 +5,6 @@ from main import check_sign, describe_number
 def test_check_sign():
     assert check_sign(10) == "positive"
     assert check_sign(-10) == "negative"
-    assert check_sign(0) == "zero"
 
 def test_describe_number():
     assert describe_number(10) ==  "The number 10 is positive."
