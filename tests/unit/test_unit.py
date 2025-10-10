754676ac29600d50a8df16f21aaa97905a46d8c8
HansLu2025 <B11209013@gapps.ntust.edu.tw>
2025-10-10 22:00:38 +0800

    Add python test file

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -1,6 +1,6 @@
 import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src")))
-from main import check_sign, describe_number
+from main import check_sign
 
 def test_check_sign():
     assert check_sign(10) == "positive"
