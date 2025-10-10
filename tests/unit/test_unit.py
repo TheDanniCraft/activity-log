530a9a19100efd2a0263f641b7aa9f51c7a29c6f
HansLu2025 <B11209013@gapps.ntust.edu.tw>
2025-10-10 22:33:53 +0800

    Add python test file

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -6,3 +6,8 @@ def test_check_sign():
     assert check_sign(10) == "positive"
     assert check_sign(-10) == "negative"
     assert check_sign(0) == "zero"
+
+def test_describe_number():
+    assert describe_number(10) ==  "The number 10 is positive."
+    assert describe_number(-10) ==  "The number -10 is negative."
+    assert describe_number(0) ==  "The number 0 is zero."
