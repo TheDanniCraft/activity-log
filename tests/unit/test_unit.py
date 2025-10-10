3425832924970ae9a6946ff6470ac6bd096f9337
HansLu2025 <B11209013@gapps.ntust.edu.tw>
2025-10-10 21:52:09 +0800

    Add python test file

--- tests/unit/test_unit.py
+++ tests/unit/test_unit.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../src")))
 from main import check_sign, describe_number
 
-def test_describe_number():
-    assert describe_number(10) ==  "The number 10 is positive."
-    assert describe_number(-10) ==  "The number -10 is negative."
-    assert describe_number(0) ==  "The number 0 is zero."
+def test_check_sign():
+    assert check_sign(10) == "positive"
+    assert check_sign(-10) == "negative"
+    assert check_sign(0) == "zero"
