0a05a53aaad0df834e7fe034dbda9b1bcd73cc39
HansLu2025 <B11209013@gapps.ntust.edu.tw>
2025-10-10 20:49:37 +0800

    Add python test file

--- tests/unit/test_example.py
+++ tests/unit/test_example.py
@@ -3,8 +3,8 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../s
 from main import check_sign, describe_number
 
 def test_check_sign():
-    assert check_sign(10) == "negative"
-    assert check_sign(-10) == "positive"
+    assert check_sign(10) == "positive"
+    assert check_sign(-10) == "negative"
     assert check_sign(0) == "zero"
 
 def test_describe_number():
